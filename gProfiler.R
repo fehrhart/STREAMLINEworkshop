d568e74c5482ffde64ce1836c97c54099d027203
Friederike (Freddie) Ehrhart <friederike.ehrhart@gmail.com>
2023-12-04 17:35:15 +0100

    Add files via upload

--- gProfiler.R
+++ gProfiler.R
@@ -12,18 +12,20 @@ BiocManager::install("gprofiler2")
 library(gprofiler2)
 
 #Set the working directory by copy-pasting your path
-setwd("C:/Users/friederike.ehrhart/Documents/Teaching/Workshop materials/Dataset GSE106589")
+setwd("C:/Users/friederike.ehrhart/Documents/Teaching/Workshop materials/Dataset GSE106589 and DESeq2 tutorial")
 
 
-#import data
-data = read.csv(file = 'GSE106589_geneCounts.csv', row.names = 1, header = TRUE)
+#import data from file
+data = read.csv(file = 'SigUP_F.csv', header = TRUE)
 
-gostres <- gost(query = c("X:1000:1000000", "rs17396340", "GO:0005005", "ENSG00000156103", "NLRP1"),
+#run the function "gostres" using the first column (named "X") containing the Ensembl IDs from the dataset
+gostres <- gost(query = data$X,
                 organism = "hsapiens")
 
 # The result is a named list where “result” is a data.frame with the enrichment analysis results
 # and “meta” containing a named list with all the metadata for the query.
 head(gostres$result)
+publish_gosttable(gostres, filename = "gProfiler_result_UP_F.pdf")
+publish_gostplot(gostplot(gostres, capped = FALSE, interactive = FALSE, filename = "gProfiler_plot_UP_F.pdf"))
 
-p <- gostplot(gostres, capped = FALSE, interactive = FALSE)
-p
+
